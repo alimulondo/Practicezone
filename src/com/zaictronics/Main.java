ee921663add21fd2ec23999c94c1667c253c6cbc
alimulondo <myalimul@gmail.com>
2021-11-30 07:59:51 +0300

    Reduce time complexity to O(n)

--- src/com/zaictronics/Main.java
+++ src/com/zaictronics/Main.java
@@ -14,26 +14,31 @@ public class Main {
     public static void main(String[] args) {
 
 	// sample array
-        int[] test = {9, 5, 2, -1, -1, 4, 1, 22, 6, 19};
+        int[] test = {9, 5, 2, -1,-1,1, 1, 4, 22, 6, 19};
+
+
 
         System.out.println(secondSmallest(test));
     }
 
     public static int secondSmallest(int[] arr){
-        int smallest = arr[0];
-        int secondsmallest = arr[1];
+
+        if(arr == null || arr.length < 2){
+            throw new IllegalArgumentException("Array is to small");
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        int secondsmallest = Integer.MAX_VALUE;
 
         for(int val:arr){
             if(val < smallest){
+                secondsmallest = smallest;
                 smallest = val;
             }
-
-        }
-
-        for (int val: arr){
-            if (val < secondsmallest && val > smallest){
+            else if(val > smallest && val < secondsmallest){
                 secondsmallest = val;
             }
+
         }
 
         return secondsmallest;
